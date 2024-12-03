c73f336d58b8037cc6e15ce5fd3175c854ffcc79
Vianne-Chen <cyy208@hotmail.com>
2024-12-03 13:11:38 -0500

    updated H_quizend page
    when host click the drink image to end the quiz section, will show the quiz has ended. also allow host to click the leaderboard button the check the participants result rank

--- client/src/components/H_QuizEnd.jsx
+++ client/src/components/H_QuizEnd.jsx
@@ -1,40 +1,4 @@
-// import React, { useEffect } from "react";
-// import '../index.css';
-// import H_Navbar from "./H_Navbar";
-// import { Link, useNavigate } from 'react-router-dom';
-// import Taps from '../assets/taps.png'; 
 
-// const H_QuizEnd = () => {
-//   useEffect(() => {
-//     document.title = "Trivia Taps - Host Quiz End";
-//   }, []);
-
-//   return (
-//     <div className="shared-screen">
-     
-//       <div className="yellow-text">
-//         Quiz Catagory Here 
-//       </div>
-//       <div className="yellow-text">
-//         That is the last question!
-//       </div>
-// {/* THIS WILL NEED TO CHANGE  */}
-//       <div className="image-container">
-//         <Link to="/ParticipantLeaderboard"> 
-//           <img src={Taps} alt="Trivia" className="center-image" />
-//         </Link>
-//       </div>
-      
-//       <p>Click the drinks to close the quiz for all users!</p>
-//       <div className="role-buttons">
-//         <Link to="/HostGameStats" className="role-button">Game Stats</Link>
-//         <Link to="/HostLeaderboard" className="role-button">View Leaderboard</Link>
-
-//       </div>
-//       <H_Navbar />
-//     </div>
-//   );
-// };
 
 // export default H_QuizEnd;
 import React, { useEffect, useState } from "react";
@@ -67,17 +31,15 @@ const H_QuizEnd = () => {
       console.error("Error ending the quiz:", error.message);
     }
 
-    // Redirect to the ParticipantLeaderboard or any other page you'd like
-    setTimeout(() => {
-      navigate("/HostQuizSelection");
-    }, 5000); // Delay the navigation slightly for a smooth experience
+    // // Redirect to the ParticipantLeaderboard or any other page you'd like
+    // setTimeout(() => {
+    //   navigate("/HostQuizSelection");
+    // }, 5000); // Delay the navigation slightly for a smooth experience
   };
 
   return (
     <div className="shared-screen">
-      <div className="yellow-text">
-        Quiz Category Here
-      </div>
+    
       <div className="yellow-text">
         That is the last question!
       </div>
